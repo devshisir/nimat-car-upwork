af130b3ce5de343e0dc92ec94408ad7fc7040e47
devshisir <contact.shisirimran@gmail.com>
2023-10-06 23:24:22 +0600

    Work backup

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     console.log('I am ready');
     $('.inject').svgInject();
+    $('.toggleButton').on('click',function(){
+        $('.sidebar').toggleClass('toggle');
+        $('.app_wrapper').toggleClass('toggle');
+    })
 });
