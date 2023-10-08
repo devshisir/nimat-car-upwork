d2ff8cb53cb938ad730cf1e6bfabecf04b7a583c
devshisir <contact.shisirimran@gmail.com>
2023-10-08 14:59:38 +0600

    Car details page add

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ function watch(){
     gulp.watch('./asset/sass/*.scss', style);
     gulp.watch('./asset/sass/components/*.scss', style);
     gulp.watch('./asset/sass/import/*.scss', style);
-    gulp.watch('./asset/sass/page/*.scss', style);
+    gulp.watch('./asset/sass/pages/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./asset/css/*.css').on('change', browserSync.reload);
     gulp.watch('./asset/js/*.js').on('change', browserSync.reload);
